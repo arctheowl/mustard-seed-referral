7d337dd22bdca4d27fe67763a1a30d5ac9eb2c35
lidevlin <lidevlin95@gmail.com>
2025-01-08 00:07:19 +0000

    fixed no vars issue

--- app/components/countDown.tsx
+++ app/components/countDown.tsx
@@ -5,19 +5,19 @@ import "@leenguyen/react-flip-clock-countdown/dist/index.css";
 import { useEffect, useState } from "react";
 import { getCountDownTime } from "../actions";
 
-interface ICountDownTime {
-  id: number;
-  time: string;
-}
+// interface ICountDownTime {
+//   id: number;
+//   time: string;
+// }
 
 export default function CountDown({ children }: any) {
-  const [data, setData] = useState<string>("2025-01-06 21:49:00");
-  // useEffect(() => {
-  //   getCountDownTime().then((data: any) => {
-  //     console.log(data);
-  //     setData(data[0].time);
-  //   });
-  // }, []);
+  const [data, setData] = useState<string>("2025-01-08 21:49:00");
+  useEffect(() => {
+    getCountDownTime().then((data: any) => {
+      console.log(data);
+      setData(data[0].time);
+    });
+  }, []);
 
   return (
     <FlipClockCountdown to={data} className="text-black">
