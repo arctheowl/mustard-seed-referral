bd06960c4709cdd12c75c66b15d9aa4c58ed153c
lidevlin <lidevlin95@gmail.com>
2025-02-12 12:52:51 +0000

    removed file

--- app/components/userInfo.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import Modal from "./modal";
-
-export default function UserInfo({ name, email, eligibility }: any) {
-  console.log(name, email, eligibility);
-  return <Modal></Modal>;
-}
