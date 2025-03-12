5e10ad165d62892b125ff7e389eb035483e24ed8
lidevlin <liam.devlin@iqvia.com>
2025-03-12 10:30:01 +0000

    Updated landing page

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import CountDown from "./countDown";
+import SignInPage from "./signIn";
 
 interface BannerProps {
   message: string;
@@ -7,9 +9,14 @@ interface BannerProps {
 
 const Banner: React.FC<BannerProps> = ({ message, countdown }) => {
   return (
-    <div className="absolute flex-col flex rounded-sm text-center bg-green-700 text-white p-4 w-screen h-1/3 top-48">
+    <div className="flex-col flex rounded-sm text-center bg-green-700 text-white p-4 w-screen h-1/3 top-48">
       <div className="w-1/2 center mx-auto">
         <h1 className="text-xl">{message}</h1>
+        <div className="flex justify-center pt-10">
+          <CountDown>
+            <SignInPage />
+          </CountDown>
+        </div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import FAQs2 from "./components/faqs2";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-slate-200">
+    <div className="bg-slate-200">
       <Image
         src="/MustardSeed_Primary-Logo.png"
         alt="logo"
@@ -16,30 +16,16 @@ export default function Home() {
         height={200}
         className="left-10 top-10 absolute"
       />
+      <div>
+        <h1 className="text-center text-4xl font-bold text-black md:p-24 sm:p-12 p-2">
+
+        </h1>
+      </div>
       <Banner
         message="There is no need to refresh this page, you will automatically receive a random queue position when the countdown finishes. Please do not navigate away. Users who join the queue after the countdown has finished will be added to the end of the queue."
         countdown={10}
       />
-      <main className="row-start-2 text-black pt-48 mx-auto place-items-center">
-        <CountDown>
-          <div>
-            <SignInPage />
-          </div>
-        </CountDown>
-
-        <div className="text-black pt-24 w-full grid grid-cols-1 gap-4 place-items-center">
-          {/* <div className="col-span-1">
-            <Image
-              src="https://mustardseedautism.co.uk/wp-content/uploads/2024/08/pic.png"
-              alt="logo"
-              layout='fill'
-              objectFit='contain'
-              className="rounded-lg h-60 w-60"
-            />
-          </div> */}
-          <FAQs2 />
-        </div>
-      </main>
+      <FAQs2 />
       <footer className="row-start-3 text-center text-black">
         <p>© 2025 Mustard Seed Autism Referral Website</p>
       </footer>
