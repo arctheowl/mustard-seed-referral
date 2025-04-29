539480e2c16f06730fb4de833cb4f344b93fa43c
lidevlin <lidevlin95@gmail.com>
2025-04-29 21:54:07 +0100

    update

--- app/components/faqs2.tsx
+++ app/components/faqs2.tsx
@@ -3,24 +3,25 @@ const ConstFAQs = [
     id: 1,
     question: "When Will I hear back from you?",
     answer:
-      "The Mustard Seed Autism Trust team will aim to get back to you within 24 hours.",
+      "The Mustard Seed Autism team will aim to confirm you appilication by the end of day Friday. If you have not heard from us by then, please check your spam folder. The team will then allocate places and you will find out if you are successful after two weeks.",
   },
   {
     id: 2,
     question: "Is there a paid for service available?",
-    answer:
-      `Yes! You can find out more about our paid for service by visiting Branches. Click the link below to find out more.`,
+    answer: `Yes! You can find out more about our paid for service by visiting Branches. Click the link below to find out more.`,
     link: "https://mustardseedautism.co.uk/branches/",
   },
   {
     id: 3,
     question: "What Information do I need to provide?",
-    answer: "You will need to provide your name, email address and a brief description of your enquiry.",
+    answer:
+      "You will need to provide your name, email address and the name of your child. You will also need to provide a brief description of your child's needs and any other relevant information.",
   },
   {
     id: 4,
     question: "What happens if I don't get a space?",
-    answer: "If you don't get a space, you will be added to the waitlist and we will contact you if a space becomes available.",
+    answer:
+      "If you don't get a space, you will be added to the waitlist and we will contact you if a space becomes available. Alternatively you can look at our paid Occupational Therapy services, Branches.",
   },
 ];
 
@@ -28,15 +29,26 @@ export default function FAQs2() {
   return (
     <div className="bg-slate-200 w-5/6 col-span-2">
       <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
-        <h2 className="text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Frequently asked questions</h2>
+        <h2 className="text-3xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+          Frequently asked questions
+        </h2>
         <dl className="mt-20 divide-y divide-gray-900/10">
           {ConstFAQs.map((faq) => (
-            <div key={faq.id} className="py-8 first:pt-0 last:pb-0 lg:grid lg:grid-cols-12 lg:gap-8">
-              <dt className="text-base/7 font-semibold text-gray-900 lg:col-span-5">{faq.question}</dt>
+            <div
+              key={faq.id}
+              className="py-8 first:pt-0 last:pb-0 lg:grid lg:grid-cols-12 lg:gap-8"
+            >
+              <dt className="text-base/7 font-semibold text-gray-900 lg:col-span-5">
+                {faq.question}
+              </dt>
               <dd className="mt-4 lg:col-span-7 lg:mt-0">
                 <p className="text-base/7 text-gray-600">{faq.answer}</p>
                 {faq.link && (
-                  <a href={faq.link} target="_blank" className="text-blue-500 hover:underline">
+                  <a
+                    href={faq.link}
+                    target="_blank"
+                    className="text-blue-500 hover:underline"
+                  >
                     Learn more
                   </a>
                 )}
@@ -46,6 +58,5 @@ export default function FAQs2() {
         </dl>
       </div>
     </div>
-  )
+  );
 }
-
--- app/page.tsx
+++ app/page.tsx
@@ -15,12 +15,10 @@ export default function Home() {
         className="left-10 top-10 absolute"
       />
       <div>
-        <h1 className="text-center text-4xl font-bold text-black md:p-24 sm:p-12 p-2">
-
-        </h1>
+        <h1 className="text-center text-4xl font-bold text-black md:p-24 sm:p-12 p-2"></h1>
       </div>
       <Banner
-        message="There is no need to refresh this page, you will automatically receive a random queue position when the countdown finishes. The wait room will be live at 6:00 PM on the 30th of April. Please do not navigate away after this time."
+        message="There is no need to refresh this page, you will automatically receive a random queue position when the countdown finishes. The wait room will be live at 7:00 PM on the 30th of April. At 8:00 PM the countdown will finish and you will be randomly assigned a queue position. You will then be able to complete the referral form if prompted to do so. If you are not taken to a form, you will be asked to join the waitlist for future consideration."
         countdown={10}
       />
       <FAQs2 />
