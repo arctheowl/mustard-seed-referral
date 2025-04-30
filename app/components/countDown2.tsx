eae2c55c239a7079e6781ccbaebe073567b98354
lidevlin <lidevlin95@gmail.com>
2025-04-30 18:04:12 +0100

    fix timer

--- app/components/countDown2.tsx
+++ app/components/countDown2.tsx
@@ -2,10 +2,16 @@
 
 import FlipClockCountdown from "@leenguyen/react-flip-clock-countdown";
 import "@leenguyen/react-flip-clock-countdown/dist/index.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { getCountDownTime } from "../actions";
 
 export default function CountDown2({ children }: any) {
   const [data, setData] = useState<string>("2025-04-30 21:49:00");
+  useEffect(() => {
+    getCountDownTime().then((data: any) => {
+      setData(data[0].time);
+    });
+  }, []);
 
   return (
     <FlipClockCountdown to={data} className="text-black">
