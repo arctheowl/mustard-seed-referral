8cd5380410d018a6b9f10c6a4acf666885d24363
lidevlin <liam.devlin@iqvia.com>
2025-04-30 12:20:32 +0100

    Added analytics

--- app/submitted/page.tsx
+++ app/submitted/page.tsx
@@ -1,31 +1,5 @@
-// const SubmittedPage = () => {
-//   return (
-//     <div className="text-center bg-slate-200 text-black h-screen flex flex-col justify-center items-center">
-//       <div className="text-4xl">Thank You!</div>
-//       <div className="text-2xl">Your form has been successfully submitted.</div>
-//       <div className="text-2xl">
-//         We aim to provide an email confirmation by the end of day tomorrow.
-//       </div>
-//       <div className="text-2xl">Final place allocations will take 2 weeks</div>
-//       <div className="text-2xl">
-//         Visit us at{" "}
-//         <a
-//           href="https://mustardseedautism.co.uk/"
-//           target="_blank"
-//           className="text-blue-500"
-//         >
-//           MustardSeedAutism
-//         </a>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default SubmittedPage;
-
 "use client";
 import Image from "next/image";
-import BannerNoCountdown from "../components/bannerNoCountdown";
 import Link from "next/link";
 
 export default function SubmittedPage() {
