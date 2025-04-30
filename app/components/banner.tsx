b0540ccc97d0959d17333be93978a0e00b1f467d
lidevlin <liam.devlin@iqvia.com>
2025-04-30 13:31:17 +0100

    Updated countdown to support mobile

--- app/components/banner.tsx
+++ app/components/banner.tsx
@@ -7,8 +7,8 @@ interface BannerProps {
 
 const Banner: React.FC<BannerProps> = ({ message }) => {
   return (
-    <div className="flex-col flex rounded-sm text-center bg-green-700 text-white p-4 w-screen h-1/3 top-48">
-      <div className="w-1/2 center mx-auto">
+    <div className="md:flex-col md:flex md:rounded-sm md:text-center bg-green-700 text-white md:p-4 md:w-screen md:h-1/3 md:top-48 ">
+      <div className="p-4 md:p-0 md:w-1/2 md:center md:mx-auto">
         <h1 className="text-xl">{message}</h1>
         <div className="flex justify-center pt-10">
           <CountDown />
--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,9 @@ export default function Home() {
         alt="logo"
         width={200}
         height={200}
-        className="left-10 top-10 absolute"
+        className="md:left-10 md:top-10 md:absolute"
       />
-      <div>
+      <div className="invisible md:visible">
         <h1 className="text-center text-4xl font-bold text-black md:p-24 sm:p-12 p-2"></h1>
       </div>
       <Banner message="There is no need to refresh this page, you will automatically receive a random queue position when the countdown finishes. The wait room will be live at 7:00 PM on the 30th of April. At 8:00 PM the countdown will finish and you will be randomly assigned a queue position. You will then be able to complete the referral form if prompted to do so. If you are not taken to a form, you will be asked to join the waitlist for future consideration." />
